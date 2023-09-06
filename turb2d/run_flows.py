fix the calculation of sediment diffusion
dt/dx terms are added in the calculation of sediment diffusion in turb2d.py.
Also, when run_multiflow_script.py is run, the file name and directory name are determined in config.yml.
In addition, the image of the sediment distribution up to the specified run no. is output at the end of the calculation.
An error message is output when the sediment entrainment function is specified or misspelled, which is not implemented.

turb2d/run_flows.py
@@ -15,6 +15,7 @@ import multiprocessing as mp
 import netCDF4
 from landlab.io.native_landlab import save_grid
 from landlab import FieldError
+import pandas as pd
 import pdb
 
 class RunMultiFlows():
@@ -75,13 +76,13 @@ class RunMultiFlows():
                     conc = np.random.uniform(Cmin, Cmax, 4)
                     salt = np.array([np.random.uniform(saltmin, saltmax)])
                     C_ini.append(np.append(conc, salt))
-            np.savetxt(os.path.join(dirpath, 'C_ini.csv'), C_ini, delimiter=',')
+            # np.savetxt(os.path.join(dirpath, 'C_ini.csv'), C_ini, delimiter=',')
 
             if hmin == hmax:
                 h_ini = np.full(num_runs, hmin)
             else:
                 h_ini = np.random.uniform(hmin, hmax, num_runs)
-            np.savetxt(os.path.join(dirpath, 'h_ini.csv'), h_ini, delimiter=',')
+            # np.savetxt(os.path.join(dirpath, 'h_ini.csv'), h_ini, delimiter=',')
             
             if rmin is None:
                 r_ini = np.empty((num_runs, ))
@@ -90,37 +91,53 @@ class RunMultiFlows():
                 r_ini = np.full(num_runs, rmin)
             else:
                 r_ini = np.random.uniform(rmin, rmax, num_runs)
-            np.savetxt(os.path.join(dirpath, 'r_ini.csv'), r_ini, delimiter=',')
+            # np.savetxt(os.path.join(dirpath, 'r_ini.csv'), r_ini, delimiter=',')
 
             if Umax == Umin:
                 U_ini = np.full(num_runs, Umin)
             else:
                 U_ini =np.random.uniform(Umin, Umax, num_runs)
-            np.savetxt(os.path.join(dirpath, 'U_ini.csv'), U_ini, delimiter=',')
+            # np.savetxt(os.path.join(dirpath, 'U_ini.csv'), U_ini, delimiter=',')
             
             if endmin == endmax:
                 endtime = np.full(num_runs, endmin)
             else:
                 endtime = np.random.randint(endmin, endmax, num_runs)
-            np.savetxt(os.path.join(dirpath, 'endtime.csv'), endtime, delimiter=',')
+            # np.savetxt(os.path.join(dirpath, 'endtime.csv'), endtime, delimiter=',')
 
             if Cfmin == Cfmax:
                 Cf = np.full(num_runs, Cfmin)
             else:
                 Cf = np.random.uniform(Cfmin, Cfmax, num_runs)
-            np.savetxt(os.path.join(dirpath, 'Cf.csv'), Cf, delimiter=',')
+            # np.savetxt(os.path.join(dirpath, 'Cf.csv'), Cf, delimiter=',')
 
             if alpha_4eqmin == alpha_4eqmax:
                 alpha_4eq = np.full(num_runs, alpha_4eqmin)
             else:
                 alpha_4eq = np.random.uniform(alpha_4eqmin, alpha_4eqmax, num_runs)
-            np.savetxt(os.path.join(dirpath,"alpha_4eq.csv"), alpha_4eq, delimiter=',')
+            # np.savetxt(os.path.join(dirpath,"alpha_4eq.csv"), alpha_4eq, delimiter=',')
 
             if r0min == r0max:
                 r0 = np.full(num_runs, r0min)
             else:
                 r0 = np.random.uniform(r0min, r0max, num_runs)
-            np.savetxt(os.path.join(dirpath, "r0.csv"), r0, delimiter=',')
+            # np.savetxt(os.path.join(dirpath, "r0.csv"), r0, delimiter=',')
+
+            df = pd.DataFrame({'h_ini': h_ini,
+                               'r_ini': r_ini,
+                               'u_ini': U_ini,
+                               'Cf': Cf,
+                               'r0': r0,
+                               'alpha_4eq': alpha_4eq,
+                               'duration': endtime
+                               })
+            column_name = []
+            for i in range(config['run_param']['grain_class_num']):
+                name = 'C{}'.format(i)
+                column_name.append(name)
+            df_conc = pd.DataFrame(C_ini, columns=column_name)
+            df_ini_param = pd.concat([df_conc, df], axis=1)
+            df_ini_param.to_csv(os.path.join(dirpath, 'ini_param.csv'), mode='x', na_rep='NaN')
 
             self.C_ini = C_ini
             self.U_ini = U_ini
@@ -343,7 +360,7 @@ class RunMultiFlows():
 
             if self.timelimit is None:
                 while (((np.sum(tc.Ch) / Ch_init) > 0.01) and (t < init_values[4])):
-                    tc.run_one_step(dt=dt)
+                    tc.run_one_step(dt=dt, repeat=i, last=t+1)
                     t += dt
 
             elif type(self.timelimit) is int:
@@ -354,7 +371,7 @@ class RunMultiFlows():
                         while (((np.sum(tc.Ch) / Ch_init) > 0.01) and (t < init_values[4])):
                             # print(t)
                             try:
-                                tc.run_one_step(dt=dt)
+                                tc.run_one_step(dt=dt, repeat=i, last=t+1)
                                 t += dt
                             except RuntimeWarning as e:
                                 print("runtimewarning!")
@@ -520,7 +537,7 @@ class RunMultiFlows():
 
         # run flows using multiple processors
         l = mp.Lock()
-        pool = mp.Pool(self.processors, initializer=self.init, initaergs=(l,))
+        pool = mp.Pool(self.processors, initializer=self.init, initargs=(l,))
         pool.map(self.run_flow, init_value_list)
         pool.close()
         pool.join()

turb2d/turb2d.py
@@ -971,12 +971,12 @@ class TurbidityCurrent2D(Component):
                     try:
                         sed_volume_grad_anchor[j] = self.bed_thick_i[i, :][self.fixed_grad_anchor_nodes][j]/self.bed_thick[self.fixed_grad_anchor_nodes][j]
                     except RuntimeWarning:
-                        sed_volume_grad_anchor[j] = 0
-                for j in range(len(sed_volume_fixed_anchor)):
+                        sed_volume_grad_anchor[j] = 0.
+                for k in range(len(sed_volume_fixed_anchor)):
                     try:
-                        sed_volume_fixed_anchor[j] = self.bed_thick_i[i, :][self.fixed_value_anchor_nodes][j]/self.bed_thick[self.fixed_value_anchor_nodes][j]
+                        sed_volume_fixed_anchor[k] = self.bed_thick_i[i, :][self.fixed_value_anchor_nodes][k]/self.bed_thick[self.fixed_value_anchor_nodes][k]
                     except RuntimeWarning:
-                        sed_volume_fixed_anchor[j] = 0
+                        sed_volume_fixed_anchor[k] = 0.
                 self.bed_thick_i[i, :][self.fixed_grad_nodes] = self.bed_thick[self.fixed_grad_nodes]*sed_volume_grad_anchor
                 self.bed_thick_i[i, :][self.fixed_value_nodes] = self.bed_thick[self.fixed_value_nodes]*sed_volume_fixed_anchor
 
@@ -1578,7 +1578,6 @@ class TurbidityCurrent2D(Component):
             U=self.U_temp,
         )
         self.Kh_temp[self.wet_pwet_links[self.Kh_temp[self.wet_pwet_links] < 0]] = 0.0
-        # pdb.set_trace()?
         # self.Fr[self.wet_pwet_links] = self.U[self.wet_pwet_links]/(self.g*self.h_link[self.wet_pwet_links])**0.5
         # print(Fr)
         # development of turbulent kinetic energy
@@ -2079,6 +2078,7 @@ class TurbidityCurrent2D(Component):
         ws = self.ws
         r0 = self.r0
 
+
         # copy previous values of Ch
         self.Ch_i_prev[:, nodes] = Ch_i[:, nodes]
         # Calculate shear velocity
@@ -2090,14 +2090,14 @@ class TurbidityCurrent2D(Component):
 
 
         # Calculate entrainment rate
-        
+        # pdb.set_trace()
         self.es[:, nodes] = get_es(
             self.R,
             self.g,
             self.Ds,
             self.nu,
             u_star,
-            v_node[nodes], 
+            U_node[nodes], 
             h[nodes], 
             r0,
             function=self.sed_entrainment_func,
@@ -2223,8 +2223,9 @@ class TurbidityCurrent2D(Component):
         """diffusion sediment transport in the region where slope is close to angle
              of repose
         """
-        diffusion_coeff_slope = 1.0e-3
+        diffusion_coeff_slope = 1.0e-5
         high_slope = 0.2
+        dx = self.dx
 
         high_slope_horizontal_links = (
             np.abs(self.S[self.wet_pwet_horizontal_links]) > high_slope
@@ -2235,7 +2236,7 @@ class TurbidityCurrent2D(Component):
         west_node = self.west_node_at_horizontal_link[
             self.wet_pwet_horizontal_links[high_slope_horizontal_links]
         ]
-        horiz_change = diffusion_coeff_slope * (
+        horiz_change = self.dt_local/self.dx*diffusion_coeff_slope * (
             self.eta[east_node] * self.bed_active_layer[:, east_node]
             - self.eta[west_node] * self.bed_active_layer[:, west_node]
         )
@@ -2250,7 +2251,7 @@ class TurbidityCurrent2D(Component):
         south_node = self.south_node_at_vertical_link[
             self.wet_pwet_vertical_links[high_slope_vertical_links]
         ]
-        vert_change = diffusion_coeff_slope * (
+        vert_change = self.dt_local/self.dx*diffusion_coeff_slope * (
             self.eta[north_node] * self.bed_active_layer[:, north_node]
             - self.eta[south_node] * self.bed_active_layer[:, south_node]
         )